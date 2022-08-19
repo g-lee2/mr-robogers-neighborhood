5033c645aa5bea852d406c2ccd0568b905179010
Grace Lee <gracexsnoopy@gmail.com>
2022-08-19 13:01:02 -0700

    WIP: try to pass fifth test and replace double digit number with message

--- js/scripts.js
+++ js/scripts.js
@@ -5,27 +5,28 @@ function returnNumber(number) {
   const returnNumberArray = []
   
   for (let i = 0; i <= number; i++) {
-    returnNumberArray.push(i);
+    returnNumberArray.push(i.toString());
     }
     console.log(returnNumberArray);
     for (let i = 0; i < returnNumberArray.length; i++) {
-      if (returnNumberArray[i] === 1) {
-        returnNumberArray[i] = "Beep!"
+      if (returnNumberArray.includes("1")) {
+        returnNumberArray["1"] = "Beep!"
         console.log(returnNumberArray)
       }
     }
       for (let i = 0; i < returnNumberArray.length; i++) {
-        if (returnNumberArray[i] === 2) {
-          returnNumberArray[i] = "Boop!"
+        if (returnNumberArray.includes("2")) {
+          returnNumberArray["2"] = "Boop!"
           console.log(returnNumberArray)
         }
       }
         for (let i = 0; i < returnNumberArray.length; i++) {
-          if (returnNumberArray[i] === 3) {
-            returnNumberArray[i] = "Won't you be my neighbor?"
+          if (returnNumberArray.includes("3")) {
+            returnNumberArray["3"] = "Won't you be my neighbor?"
             console.log(returnNumberArray)
           }
         };
 }
+
 
  
