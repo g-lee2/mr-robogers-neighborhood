7c3f5054f846666d72437cf3f4350fbc8a7b5cb2
Grace Lee <gracexsnoopy@gmail.com>
2022-08-19 17:05:54 -0700

    rewrite first test and try to pass test

--- README.md
+++ README.md
@@ -50,3 +50,10 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 Copyright (c) _2022_ _Grace Lee_
+
+Describe: returnNumberArray()
+
+Test: "It should return an array of numbers from 0 to the inputted number"
+Code: returnNumber(5);
+Expected Output: [0, 1, 2, 3, 4, 5]
+ 
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,9 @@
-function returnNumberArray(input) {
-  const numberArray = [];
-  for (let i = 0; i <= input; i++) {
-    numberArray.push(i);
-  } 
-  return numberArray;
-} 
 
-function replaceNumber(numberArray, target, replacement) {
-  for (let i = 0; i < numberArray.length; i++) {
-    if (numberArray[i].toString().includes("1")) {
-      numberArray[i] = "Beep!";
-    }
-  }
-  return numberArray;
-}
+
+
+
+
+
+
 
 
