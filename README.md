ae146f78dd22013e32bfc91ca500c7b276f909d2
Grace Lee <gracexsnoopy@gmail.com>
2022-08-19 17:23:56 -0700

    pass test two and replace 1 with beep

--- README.md
+++ README.md
@@ -59,3 +59,10 @@ Test: "It should return an array of numbers from 0 to the inputted number"
 Code: returnNumber(5);
 Expected Output: [0, 1, 2, 3, 4, 5]
  
+Test: "It should replace 1 with "Beep!"
+Code: returnNumber(5);
+Expected Output: [0, "Beep!", 2, 3, 4, 5]
+
+Test: "It should replace 2 with "Boop!"
+Code: returnNumber(5);
+Expected Output: [0, "Beep!", "Boop!", 3, 4, 5]
--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,18 @@ function returnNumberArray(input) {
   for (let i = 0; i <= input; i++) {
     numberArray.push(i);
   } 
+  for (let i = 0; i < numberArray.length; i++) {
+    if (numberArray[i].toString().includes("1")) {
+      numberArray[i] = "Beep!";
+    }
+  }
+  for (let i = 0; i < numberArray.length; i++) {
+    if (numberArray[i].toString().includes("2")) {
+      numberArray[i] = "Boop!";
+    }
+  }
   return numberArray;
-} 
-
+}
 
 
 
